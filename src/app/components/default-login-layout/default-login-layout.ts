becc29d3bde4694cfddd6875e70cb81c3a77e787
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-10-25 15:43:45 -0300

    tela principl concluida - iniciar construção dos forms

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './default-login-layout.html',
   styleUrl: './default-login-layout.scss',
 })
 export class DefaultLoginLayout {
-
+    @Input() title: string = '';
+    @Input() primaryBtnText: string = '';
+    @Input() secondaryBtnText: string = '';
 }
