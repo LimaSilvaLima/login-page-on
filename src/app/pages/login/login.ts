aea694ac80448b2c73fa56cd7a57b6091baa0ea0
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-10-26 19:45:45 -0300

    trabalhando form de User

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
    standalone: true,
   imports: [
-    DefaultLoginLayout
+    DefaultLoginLayout,
+    ReactiveFormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+  loginFormm!: FormGroup;
+  constructor() {
+    this.loginFormm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });  
+  }
 }
